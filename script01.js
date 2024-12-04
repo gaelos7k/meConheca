ef64a853a664975beb9b7fa61106821f0ade9d41
Gabriel Henrique <l77gabrielgh77@hotmail.com>
2024-12-04 20:57:18 -0300

    first commit

---
+++ script01.js
@@ -0,0 +1,6 @@
+let resposta = prompt("Para me conhecer melhor digite seu nome");
+alert("Fico feliz que queira saber mais sobre mim, " + resposta);
+
+function soma(n1, n2) {
+    return n1 + n2;
+}
